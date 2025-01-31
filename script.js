259097604912f0c71f2f2024ca9fcbf05b688ad6
Saiman Saxena <saimanhero1555@gmail.com>
2025-01-31 13:06:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-// Get input field and button
-const videourlinput = document.getElementById("video-url");
-const fetchbutton = document.getElementById("detail-button");
+function getCurrentPage() {
+  const bodyid = document.body.id;
 
-// Check if elements exist before adding an event listener
-if (videourlinput && fetchbutton) {
-  fetchbutton.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevents form submission from reloading the page
+  if(bodyid === 'index-page'){
+    const videourlinput = document.getElementById("video-url");
+    const fetchbutton = document.getElementById("detail-button");
 
-    // Get the URL from the input field and remove extra spaces
-    const videourl = videourlinput.value.trim();
+     
 
-    // Check if the user entered a URL
-    if (videourl === "") {
+    const videoUrl = videourlinput.value.trim();
+
+    const urlformat = "/^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]{11}(&.*)?$/";
+
+    if(videoUrl ===""){
       alert("Please enter a YouTube URL.");
-      return;
+      return ;
     }
 
-    // Regular expression to validate YouTube URL
-    const youtubePattern =
-      /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]{11}(&.*)?$/;
 
-    // Check if the URL is NOT valid
-    if (!youtubePattern.test(videourl)) {
-      alert(
-        "Please enter a valid YouTube URL. Example: https://www.youtube.com/watch?v=VIDEO_ID"
-      );
-      return;
-    }
 
-    // Redirect user to the download page with the video URL as a parameter
-    window.location.href = `download-page.html?url=${encodeURIComponent(
-      videourl
-    )}`;
-  });
-} else {
-  console.error("Error: Input field or button not found in the document.");
+
+  }
+  else if(bodyid === 'download-page'){
+//! todo
+
+  }
+  else{
+    return 'unknown';
+  }
 }
