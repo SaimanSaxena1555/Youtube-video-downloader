bf646b53628e89e4a70b53bd61ade704583b7ef5
Saiman Saxena <saimanhero1555@gmail.com>
2025-02-03 11:26:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
 
+app.use(cors());
+
 app.use(express.static('public'));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app,get('/download',async (req,res)) => {
+    const { url, quality, audioFormat } = req.query;  
+}
 
-app.get('/',(req , res) =>{
-    res.sendFile(__dirname + '/index.html');
-})
+if(!url){
+    return res.status(400).send('Video URL is required!');
+}
 
-
+try{
+    const info = await ytdl.chooseFormat(info.format,{filter: 'audioonly', quality: audioFormat });
+    if(!format){
+        return res.status(400).send('Audio format not available.');
+    }
+    
+}
